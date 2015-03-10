41e3953e58ab872df7ed503e24217842192be747
parashardhapola <parashar.dhapola@gmail.com>
2015-03-10 20:56:36 +0530

    changes to oncotypin forms. Now using macro in html to render form. Cahnges in forms incorporated

--- oncotyping/db_repository/manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-
-if __name__ == '__main__':
-    main(six='<module 'six' from '/home/patronus/pygenome_project/venv_pygenome/lib/python3.4/site-packages/six.py'>')
--- oncotyping/onco_models.py
+++ oncotyping/onco_models.py
@@ -32,49 +32,34 @@ class User(db.Model):
 class Patient(db.Model):
 
     code = db.Column(db.String(200), primary_key=True)
-    age = db.Column(db.Integer)
-    date_of_entry = db.Column(db.DateTime)
-    place_of_collection = db.Column(db.String(200))
-
-    menopause = db.Column(db.Boolean)
-    familial_history = db.Column(db.String(200))
-    weight = db.Column(db.Integer)
-    hiv_status = db.Column(db.Boolean)
-
-    date_of_death = db.Column(db.DateTime)
-    date_of_last_contact = db.Column(db.DateTime)
-
-    tnm_staging = db.Column(db.String(200))
-    tumour_resection_date = db.Column(db.String(200))
-    tumour_type = db.Column(db.String(200))
-    tumour_site = db.Column(db.String(200))
-    remission_history = db.Column(db.String(200))
-
-    procurement_method = db.Column(db.String(200))
     pathology_report_id = db.Column(db.String(200))
+    date_of_collection = db.Column(db.DateTime)
+    date_of_resection = db.Column(db.DateTime)
+    place_of_collection = db.Column(db.String(200))
+    age = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    menopause = db.Column(db.Boolean)
+    hiv_status = db.Column(db.Boolean)
+    familial_history = db.Column(db.String(200))
     normal_sample_collected = db.Column(db.Boolean)
     normal_tissue_proximity = db.Column(db.String(200))
-    diagnosis = db.Column(db.String(200))
-
-    adjuvant_radiotherapy = db.Column(db.String(200))
-    neo_adjuvant_radiotherapy = db.Column(db.String(200))
-    adjuvant_chemotherapy = db.Column(db.String(200))
-    neo_adjuvant_chemotherapy = db.Column(db.String(200))
-    targeted_molecular_therapy = db.Column(db.String(200))
-
-    #percent_lymphocyte_infiltration = db.PositiveIntegerField(null=True, blank=True)
-    #percent_monocyte_infiltration = db.PositiveIntegerField(null=True, blank=True)
-    #percent_necrosis = db.PositiveIntegerField(null=True, blank=True)
-    #percent_neutrophil_infiltration = db.PositiveIntegerField(null=True, blank=True)
-    #percent_normal_cells = db.PositiveIntegerField(null=True, blank=True)
-    #percent_stromal_cells = db.PositiveIntegerField(null=True, blank=True)
-    #percent_tumor_cells = db.PositiveIntegerField(null=True, blank=True)
-
+    excision_method = db.Column(db.String(200))
+    tumour_type = db.Column(db.String(200))
+    tumour_site = db.Column(db.String(200))
+    tnm_staging = db.Column(db.String(200))
     er_status = db.Column(db.Boolean)
     pr_status = db.Column(db.Boolean)
     her2_status = db.Column(db.Boolean)
-
-    #rin_value = db.PositiveIntegerField(null=True, blank=True)
+    neo_adjuvant_chemotherapy = db.Column(db.String(200))
+    neo_adjuvant_radiotherapy = db.Column(db.String(200))
+    adjuvant_chemotherapy = db.Column(db.String(200))
+    adjuvant_radiotherapy = db.Column(db.String(200))
+    targeted_molecular_therapy = db.Column(db.String(200))
+    patient_alive = db.Column(db.Boolean)
+    date_of_death = db.Column(db.DateTime)
+    date_of_last_contact = db.Column(db.DateTime)
+    remission_history = db.Column(db.String(200))
+    comments = db.Column(db.String(200))
 
     def __str__(self):
         return '<Patient code %r>' % self.code
